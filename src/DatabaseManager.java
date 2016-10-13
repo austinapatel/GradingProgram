7705b612ccceb3060e4af6abc50da75a6abc386c
Austin.Patel -  110492 <Austin.Patel@TUHSD.EDU>
2016-10-13 11:55:38 -0700

    Example change in Database Manager.

--- src/DatabaseManager.java
+++ src/DatabaseManager.java
@@ -5,12 +5,31 @@
 // 10/13/16
 // DatabaseManager.java
 
+import java.sql.*;
+
 public class DatabaseManager
 {
 
 	public static void main(String[] args)
 	{
-
+		init();
+	}
+	
+	public static void init()
+	{
+		Connection c = null;
+		try
+		{
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:test.db");
+		}
+		catch (Exception e)
+		{
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+		System.out.println("Opened database successfully");
+		System.out.println("hello");
 	}
 
 }
