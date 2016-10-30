0fc212bf0cddc55f36cf1d0d948dcde31a3adc5f
BuildTools <unconfigured@null.spigotmc.org>
2016-10-29 19:04:08 -0700

    data created

--- src/DatabaseManager.java
+++ src/DatabaseManager.java
@@ -1,3 +1,5 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 // Austin Patel & Jason Morris
 // APCS
@@ -8,14 +10,36 @@
 public class DatabaseManager
 {
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws Exception
 	{
+		getConnection();
 		init();
 	}
 	
 	public static void init()
 	{
 
+	}
+	
+	public static Connection getConnection() throws Exception
+	{
+		try
+		{
+			String driver = "com.mysql.jdbc.Driver";
+			String url = "jdbc:mysql://db4free.net:3306/gradingprogram?autoReconnect=true&useSSL=false";
+			String username = "cheetahgod";
+			String password = "Prince1";
+			Class.forName(driver);
+
+			Connection conn = DriverManager.getConnection(url, username, password);
+			System.out.println("Connected");
+			return conn;
+		}catch (Exception e)
+		{
+			System.out.print(e);
+		}
+		
+		return null;
 	}
 
 }
