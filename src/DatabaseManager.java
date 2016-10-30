089a2ab034a6914c7d9d273d1a9228024b64e303
BuildTools <unconfigured@null.spigotmc.org>
2016-10-29 18:09:14 -0700

    fix

---
+++ src/DatabaseManager.java
@@ -0,0 +1,21 @@
+
+// Austin Patel
+// APCS
+// Redwood High School
+// 10/13/16
+// DatabaseManager.java
+
+public class DatabaseManager
+{
+
+	public static void main(String[] args)
+	{
+		init();
+	}
+	
+	public static void init()
+	{
+
+	}
+
+}
