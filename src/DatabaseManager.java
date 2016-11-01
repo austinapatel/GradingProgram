e49b1bc7b9fbcf637d5d5f4caff6df2e52b7fac4
cheetahgod <jasonbm123@gmail.com>
2016-10-31 20:01:10 -0700

    changed database to my sick raspberry pi mysql database yay!

--- src/DatabaseManager.java
+++ src/DatabaseManager.java
@@ -1,54 +1,59 @@
 
-// Austin Patel & Jason Morris
-// APCS
-// Redwood High School
-// 10/13/16
-// DatabaseManager.java
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
+
+
+// Austin Patel & Jason Morris
+// APCS
+// Redwood High School
+// 10/13/16
+// DatabaseManager.java
+//mysql data setup help from mysql tutorial by Steven Byrne
+
 
 public class DatabaseManager
 {
 	private static String password = "";
-	private static Connection connection;
-	
 	public static void main(String[] args) throws Exception
 	{
-		password = getPassword();
-		connection = getConnection();
-		
+		getPassword();
 		createTable();
 		
 		String[][] names = {{"Carl", "K"}, {"Zach", "J"}, {"Frank" , "B"}, {"Ken", "Mark"}};	
 		
-		// Put all the names in the database
-		for (String[] name: names)
-			post(name[0], name[1]);
 		
-		System.out.println(getStudent("Carl"));
+		for (String[] name: names)
+		{
+			post(name[0], name[1]);
+		}
+		
+		//System.out.println(Arrays.toString(getStudent("Austin").toArray()));
+		
 	}
 	
-	public static String getPassword()
+	public static void getPassword()
 	{
-		System.out.print("Enter password:  ");
-		
-		Scanner scanner = new Scanner(System.in);
-		return scanner.nextLine();
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter database password:   ");
+		password = input.nextLine();
+		input.close();
+
 	}
 	
 	public static ArrayList<String> getStudent(String var1) throws Exception
 	{
 		try
 		{
+			Connection con = getConnection();
 			//PreparedStatement statement = con.prepareStatement("SELECT first,lastname FROM tablename LIMIT 1"); 
 			/// SELECT * FROM tablename, table2 WHERE tablename.first = table2.first AND ... OR
 			//PreparedStatement statement = con.prepareStatement("SELECT * FROM tablename, table2 ORDER BY lastname ASC");
-			PreparedStatement statement = connection.prepareStatement("SELECT * FROM tablename WHERE first = '"+var1+"' LIMIT 1");
+			PreparedStatement statement = con.prepareStatement("SELECT * FROM tablename WHERE first = '"+var1+"' LIMIT 1");
 			
 			ResultSet result = statement.executeQuery();
 			
@@ -76,9 +81,10 @@ public class DatabaseManager
 		{
 			String driver = "com.mysql.jdbc.Driver";
 			String url = "jdbc:mysql://db4free.net:3306/gradingprogram?autoReconnect=true&useSSL=false";
+			String url2 = "jdbc:mysql://98.248.155.100:3306/gradingprogram?autoReconnect=true&useSSL=false";
 			String username = "cheetahgod";
 			Class.forName(driver);
-			Connection conn = DriverManager.getConnection(url, username, password);
+			Connection conn = DriverManager.getConnection(url2, username, password);
 			System.out.println("Connected");
 			return conn;
 		
@@ -94,7 +100,8 @@ public class DatabaseManager
 	{
 		try
 		{
-			PreparedStatement create = connection.prepareStatement("CREATE TABLE IF NOT EXISTS tablename(id int NOT NULL AUTO_INCREMENT, first varchar(255), lastname varchar(255), PRIMARY KEY (id))");
+			Connection con = getConnection();
+			PreparedStatement create = con.prepareStatement("CREATE TABLE IF NOT EXISTS tablename(id int NOT NULL AUTO_INCREMENT, first varchar(255), lastname varchar(255), PRIMARY KEY (id))");
 			create.executeUpdate();
 		}catch (Exception e)
 		{
@@ -115,7 +122,8 @@ public class DatabaseManager
 		
 		try
 		{
-			PreparedStatement posted = connection.prepareStatement("INSERT INTO tablename (first, lastname) VALUES ('"+var1+"', '"+var2+"')");
+			Connection con = getConnection();
+			PreparedStatement posted = con.prepareStatement("INSERT INTO tablename (first, lastname) VALUES ('"+var1+"', '"+var2+"')");
 			posted.executeUpdate();
 			
 		}catch (Exception e)
