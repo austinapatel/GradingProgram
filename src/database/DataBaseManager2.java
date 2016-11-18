a3a6fe557db28e57afeab1a94821f3e7f8744b00
BuildTools <unconfigured@null.spigotmc.org>
2016-11-18 12:26:38 -0800

    no time to type anything

---
+++ src/database/DataBaseManager2.java
@@ -0,0 +1,26 @@
+package database;
+
+import java.util.ArrayList;
+
+public class DataBaseManager2
+{
+	
+//	public static getData()
+//	{
+//		GetConnection connect = new GetConnection();
+//	}
+	
+	
+	public static ArrayList<Student> getStudents()
+	{
+		
+		
+	
+		
+		
+		
+	}
+	
+	
+
+}
