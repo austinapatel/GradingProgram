6b2d1d4995b3313aa551213e23b498f4cac2e9c1
BuildTools <unconfigured@null.spigotmc.org>
2016-11-21 09:36:13 -0800

    merge

--- src/database/DatabaseManager.java
+++ src/database/DatabaseManager.java
@@ -8,14 +8,11 @@
 
 package database;
 
-import java.io.FileInputStream;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Properties;
 import java.util.Scanner;
 
 public class DatabaseManager
