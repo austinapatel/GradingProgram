589232b9a9f39f6cf9bbc8a4908cb8e61921cb29
cheetahgod <jasonbm123@gmail.com>
2017-04-10 17:12:39 -0700

    ;

--- src/grading/GradingScale.java
+++ src/grading/GradingScale.java
@@ -19,8 +19,6 @@ public class GradingScale
 	private String name;
 	public GradingScale(String jsonText, String name)
 	{
-		if (jsonText.trim().equals(""))
-			return;
 		this.name = name;
 		try {
 			letterGrades = new JSONArray(jsonText);
