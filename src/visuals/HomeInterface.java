2cabb9591fdac8ba2f59cfac6baffdbeb534a80d
Austin Patel <austinapatel@gmail.com>
2017-05-11 11:37:17 -0700

    Fully converted to non-tab interface

--- src/visuals/HomeInterface.java
+++ src/visuals/HomeInterface.java
@@ -1,6 +1,10 @@
 package visuals;
 
+import grading.GradingScaleInterface;
+import utilities.ConsolePanel;
+
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -10,18 +14,30 @@ public class HomeInterface extends JPanel implements ActionListener {
 
     private HashMap<String, JPanel> interfaces;
     private Interface mainInterface;
+    private JPanel contentPanel;
 
     public HomeInterface(Interface mainInterface) {
         initInterfaceHashMap();
+        initPanel();
         initVisuals();
 
         this.mainInterface = mainInterface;
+    }
+
+    private void initPanel() {
+        contentPanel = new JPanel();
+        contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
+        add(contentPanel);
     }
 
     private void initInterfaceHashMap() {
         interfaces = new HashMap<String, JPanel>(){{
             put("Create Class", new CreateClassInterface());
             put("Create Assignment", new CreateAssignmentInterface());
+            put("View Tables", new TableInterface());
+            put("Console", new ConsolePanel());
+            put("Grading Scale", new GradingScaleInterface());
+            put("View Class Grades", new GradesInterface());
         }};
     }
 
@@ -34,12 +50,17 @@ public class HomeInterface extends JPanel implements ActionListener {
         for (String key : keys)
             buttons.add(new JButton(key));
 
-        add(new JLabel("Grading Program"));
-
         for (JButton button : buttons) {
             button.addActionListener(this);
-            add(button);
+            wrapInJPanel(button);
         }
+    }
+
+    private void wrapInJPanel(JComponent component) {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        contentPanel.add(panel);
+        panel.add(component);
     }
 
     @Override
