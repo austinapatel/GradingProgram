2299088174dcffce20c65188971f15207b735be9
Achinthya.Poduval -  110018 <Achinthya.Poduval@TUHSD.EDU>
2017-05-16 09:13:44 -0700

    enrollStudent capability added

--- src/visuals/EnrollmentsInterface.java
+++ src/visuals/EnrollmentsInterface.java
@@ -1,47 +1,90 @@
 package visuals;
 
-import database.TableProperties;
-
-import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.swing.JButton;
+
+import database.DataTypeManager;
+import database.Table;
+import database.TableManager;
+import database.TableProperties;
 
 /**
  * Created by austin.patel on 5/15/2017.
  */
-public class EnrollmentsInterface extends InterfacePanel {
+public class EnrollmentsInterface extends InterfacePanel
+{
 
-    private JButton enrollButton;
-    private DatabaseJTable studentsTable, coursesTable;
+	private JButton enrollButton;
+	private DatabaseJTable studentsJTable, coursesJTable;
 
-    public EnrollmentsInterface() {
-        initTables();
-    }
+	public EnrollmentsInterface()
+	{
+		initTables();
+	}
 
-    private void initTables() {
-        studentsTable = new DatabaseJTable(TableProperties.STUDENTS_TABLE_NAME);
-        coursesTable = new DatabaseJTable(TableProperties.COURSES_TABLE_NAME);
+	private void initTables()
+	{
+		studentsJTable = new DatabaseJTable(TableProperties.STUDENTS_TABLE_NAME);
+		coursesJTable = new DatabaseJTable(TableProperties.COURSES_TABLE_NAME);
 
-        add(studentsTable.getTableHeader());
-        add(studentsTable);
+		add(studentsJTable.getTableHeader());
+		add(studentsJTable);
 
-        add(coursesTable.getTableHeader());
-        add(coursesTable);
-        
-        add(enrollButton = new JButton("Enroll Student"));
-    }
+		add(coursesJTable.getTableHeader());
+		add(coursesJTable);
 
-    @Override
-    public void keyTyped(KeyEvent e) {
+		add(enrollButton = new JButton("Enroll Student"));
+		enrollButton.addActionListener(new ActionListener()
+		{
 
-    }
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				Table studentsTable = TableManager.getTable(TableProperties.STUDENTS_TABLE_NAME);
+				ArrayList<Integer> studentIds = DataTypeManager.toIntegerArrayList(studentsTable.getAllFromColumn(TableProperties.STUDENT_ID));
+				
+				int studentId = studentIds.get(studentsJTable.getSelectedRow());
+				
+				Table coursesTable = TableManager.getTable(TableProperties.COURSES_TABLE_NAME);
+				ArrayList<Integer> courseIds = DataTypeManager.toIntegerArrayList(coursesTable.getAllFromColumn(TableProperties.COURSE_ID));
 
-    @Override
-    public void keyPressed(KeyEvent e) {
+				int courseId = courseIds.get(coursesJTable.getSelectedRow());
+				
+				Table enrollmentsTable = TableManager.getTable(TableProperties.ENROLLMENTS_TABLE_NAME);
 
-    }
+				HashMap<String, Object> enrollmentsVals = new HashMap<String, Object>()
+				{
+					{
+						put(TableProperties.STUDENT_ID, studentId);
+						put(TableProperties.COURSE_ID, courseId);
+					}
+				};
 
-    @Override
-    public void keyReleased(KeyEvent e) {
+				TableManager.insertValuesIntoNewRow(enrollmentsTable, enrollmentsVals);
+			}
+		});
+	}
 
-    }
+	@Override
+	public void keyTyped(KeyEvent e)
+	{
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e)
+	{
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e)
+	{
+
+	}
 }
