f6094156190f953efff2196fb3ec68595583f05a
Austin Patel <austinapatel@gmail.com>
2017-05-23 09:52:44 -0700

    Removed bad JPanel layouts, but still is not formatted correctluy

--- src/visuals/InterfacePanel.java
+++ src/visuals/InterfacePanel.java
@@ -7,28 +7,34 @@ import java.awt.event.KeyListener;
 /**Tools for making the layout of a JPanel centered with correctly sized items.*/
 public abstract class InterfacePanel extends JPanel implements KeyListener {
 
-    private JPanel contentPanel;
+//    private JPanel contentPanel;
 
     public InterfacePanel() {
-        contentPanel = new JPanel();
+//        contentPanel = new JPanel();
         init();
-        super.add(contentPanel);
+//        super.add(contentPanel);
     }
 
     /**Initializes the base layout of a JPanel.*/
     public void init() {
-        contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
     }
 
     /**Wraps a component in its own JPanel, so that it will center align and resize
      * correctly and then adds it to a frame.*/
     @Override
     public Component add(Component toAdd) {
-        JPanel basePanel = new JPanel();
-        basePanel.setLayout(new BorderLayout());
-        basePanel.add(toAdd);
+//        JPanel basePanel = new JPanel();
+//        basePanel.setLayout(new BorderLayout());
+//        basePanel.add(toAdd);
+//
+//        return contentPanel.add(basePanel);
 
-        return contentPanel.add(basePanel);
+        Dimension size = new Dimension(1000, (int) toAdd.getPreferredSize().getHeight());
+
+        toAdd.setMaximumSize(size);
+
+        return super.add(toAdd);
     }
 
 //    @Override
