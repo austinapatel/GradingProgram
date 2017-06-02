3099f63827c2564ab6b408c08d41dbaaad537710
BuildTools <unconfigured@null.spigotmc.org>
2017-06-02 11:10:23 -0700

    changed icon

--- src/visuals/BaseInterface.java
+++ src/visuals/BaseInterface.java
@@ -101,12 +101,21 @@ public class BaseInterface extends JFrame {
      */
     private void initFrame() {
         
+   	 try
+   	 {
+   		 
+   	 
    	 if (System.getProperty("user.name").equals("jason.morris") || System.getProperty("user.name").equals("achinthya.poduval"))
    	 {
       	 setIconImage(new ImageIcon(getClass().getClassLoader().getResource("icon.png")).getImage());
    	 }
    	 else
       	 setIconImage(new ImageIcon(getClass().getClassLoader().getResource("icon2.png")).getImage());
+   	 }
+   	 catch(Exception e)
+   	 {
+   		 
+   	 }
 
    	 
         setSize(WIDTH, HEIGHT);
